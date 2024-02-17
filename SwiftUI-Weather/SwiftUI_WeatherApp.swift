dc828ffb2efc4b60da8c8b4b0fa71ea3dd8d10d6
Aoli <939677612@qq.com>
2024-02-17 01:14:22 -0600

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Shaohua Zhang on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
